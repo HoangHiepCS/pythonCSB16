2d2d954311952231f1701eb4c79cac91f7253ce8
Hoàng Hiệp <hoanghiepg6@MacBook-Air-cua-Hoang-Hiep.local>
2022-07-29 20:47:06 +0700

    upload file 29/7/2022

---
+++ b1_print.py
@@ -0,0 +1,9 @@
+# # print("Hello World!")
+# userName = "Hoang Hiep"
+# age = 15
+# address = "Viet Nam"
+# print(userName)
+# print(age)
+# print("hello my name is",userName)
+# print("toi ten la",userName,"nam nay toi",age,"dia chi nha toi la ở",address)
+print(f'ten toi la {userName},{age},{address}')
